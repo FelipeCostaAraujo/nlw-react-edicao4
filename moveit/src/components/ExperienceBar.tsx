79e3e3193cba4ffbc93454efdfbbdb11f75cb5c4
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-24 18:04:01 -0300

    refactoring experiencebar

--- moveit/src/components/ExperienceBar.tsx
+++ moveit/src/components/ExperienceBar.tsx
@@ -1,12 +1,14 @@
-export function ExperienceBar(){
-    return(
-        <header className="experience-bar">
+import styles from '../styles/components/ExperienceBar.module.css';
+
+export function ExperienceBar() {
+    return (
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%'}}></div>
-                <span className="current-experience" style={{ left: '50%'}}>
+                <div style={{ width: '50%' }}></div>
+                <span className={styles.currentExperience} style={{ left: '50%' }}>
                     300 xp
-                    </span>
+                </span>
             </div>
             <span>600 xp</span>
         </header>
