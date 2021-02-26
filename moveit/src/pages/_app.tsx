6ec2803e0daa4d8a7fbcb0c5f05022ca4aa9d668
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 01:11:34 -0300

    add CountdownProvider

--- moveit/src/pages/_app.tsx
+++ moveit/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />)
+      <CountdownProvider>
+        <Component {...pageProps} />)
+      </CountdownProvider>
     </ChallengesProvider>
   );
 }
