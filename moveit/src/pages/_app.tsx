8e0de83a3f2e24f9686c58b6e628a00e5bca2bac
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 01:13:40 -0300

    move CountdownProvider to its own file

--- moveit/src/pages/_app.tsx
+++ moveit/src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { ChallengesProvider } from '../contexts/ChallengesContext';
-import { CountdownProvider } from '../contexts/CountdownContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChallengesProvider>
-      <CountdownProvider>
-        <Component {...pageProps} />)
-      </CountdownProvider>
+      <Component {...pageProps} />)
     </ChallengesProvider>
   );
 }
