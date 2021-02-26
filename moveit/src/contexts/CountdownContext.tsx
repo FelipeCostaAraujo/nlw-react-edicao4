8e54cee4c150de80fef7e827822441807d315d2f
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 01:00:35 -0300

    add CountdownContext

---
+++ moveit/src/contexts/CountdownContext.tsx
@@ -0,0 +1,17 @@
+import { createContext, ReactNode } from "react";
+
+interface CountdownContextData { }
+
+interface CountdownProviderProps {
+    children: ReactNode
+}
+
+const CountdownContext = createContext({} as CountdownContextData);
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+    return (
+        <CountdownContext.Provider value={{}}>
+            {children}
+        </CountdownContext.Provider>
+    )
+}
